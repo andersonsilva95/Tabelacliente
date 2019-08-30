9c62c75c147506b723479db5c86004fcc4399999
andersonsilva95 <anderson_ps12@hotmail.com>
2019-08-29 21:12:39 -0300

    adicionando sql funcional

--- tabfuncional.sql
+++ tabfuncional.sql
@@ -1,25 +1,25 @@
 	use master
-go
+Go
 
 	drop database tabelarelacionada
-go
+Go
 
 	create database tabelarelacionada
-go
+Go
 
 	use tabelarelacionada
-go
+Go
 
 
 
-create table tabelavendas(
+create table tb_vendas(
        id_venda int primary key IDENTITY(1,1),
        id_cliente int not null,
-	   id_item_vendido int not null,
+	   id_itens_vendidos int not null,
        data date not null,
        desconto decimal(2,2))
 GO
-create table tabelaclientes(
+create table tb_clientes(
        id_cliente int PRIMARY KEY IDENTITY(1,1),
        nome nvarchar(80) not null,
        endereco nvarchar(120),
@@ -28,41 +28,40 @@ create table tabelaclientes(
        fone nvarchar(9),
        email nvarchar(80),)
 GO
-create table tabeladeitensvendidos(
-       id_item_vendido int PRIMARY KEY identity(1,1),
+create table tb_itens_vendidos(
+       id_itens_vendidos int PRIMARY KEY identity(1,1),
        id_produto int not null,
        id_hardware int not null,
        qtde_item int not null,
        pco_vda decimal(8,2) not null)
 GO
-create table tabeladevendascanceladas(
+create table tb_vendas_canceladas(
        id_vendacancelada int PRIMARY KEY identity(1,1),
-       id_itemvendido int not null,)
+       id_itens_vendidos int not null,)
 GO
-create table tabeladeprodutos(
+create table tb_produtos(
        id_produto int PRIMARY KEY IDENTITY(1,1),
        descricao nvarchar(80) not null,
        preco_unit decimal NOT NULL,
        qtde_atual int NOT NULL,
        qtde_minima int,)
 GO
-alter table tabeladeitensvendidos
+alter table tb_itens_vendidos
       ADD CONSTRAINT fk_id_produto
       FOREIGN KEY (id_produto) REFERENCES tb_produtos(id_produto)
 	  go
 
-	  alter table tabeladevendas
-      ADD CONSTRAINT fk_id_item_vendido
-      FOREIGN KEY (id_item_vendido) REFERENCES tb_itens_vendidos(id_item_vendido)
+	  alter table tb_vendas
+      ADD CONSTRAINT fk_id_itens_vendidos
+      FOREIGN KEY (id_itens_vendidos) REFERENCES tb_itens_vendidos(id_itens_vendidos)
 	  go
 
-	  alter table tabeladeclientes
+	  alter table tb_clientes
       ADD CONSTRAINT fk_id_cliente
       FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id_cliente)
 	  go
 
-	  alter table tabeladevendascanceladas
-      ADD CONSTRAINT fk_item_vendido
-      FOREIGN KEY (id_item_vendido) REFERENCES tb_itens_vendidos(id_item_vendido)
+	  alter table tb_vendas_canceladas
+      ADD CONSTRAINT fk_itens_vendidos
+      FOREIGN KEY (id_itens_vendidos) REFERENCES tb_itens_vendidos(id_itens_vendidos)
 	  go
-
